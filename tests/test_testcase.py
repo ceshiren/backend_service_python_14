7ceddbadc6874b9088d38c0d42d24fdd12f86b13
seveniruby <seveniruby@gmail.com>
2020-09-17 21:34:58 +0800

    脚手架搭建完成

---
+++ tests/test_testcase.py
@@ -0,0 +1,14 @@
+import requests
+import datetime
+
+def test_add():
+    r=requests.post(
+        'http://127.0.0.1:5000/testcase',
+        json={
+            'name': f'name {str(datetime.datetime.now())}',
+            'description': 'd',
+            'data': ''
+        }
+    )
+    assert r.status_code == 200
+    assert  r.json()['msg'] == 'ok'
